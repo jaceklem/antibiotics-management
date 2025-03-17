fa2e5ca4a490ee83d7a5f362449a3caaa4f02df0
jaceklem <welovejacek@gmail.com>
2025-03-17 20:03:35 +0100

    implement pseudo second order rate

--- adsorber.py
+++ adsorber.py
@@ -1,50 +1,62 @@
+import os
 import numpy as np
-import scipy.optimize as optimize
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
 class Adsorber:
-    def __init__(self, C0, PAC, Vr, Qmax, KL, k_ext, k_int, A):
+    def __init__(self, C0, qt0, Fv, Vr, Qmax, KL, k2, L, Mm, tspan):
         self.C0 = C0  # Initial concentration (mg/L)
-        self.PAC = PAC  # PAC dosage (g)
+        self.qt0 = qt0  # Initial adsorbed amount (mg/g)
+        self.Fv = Fv  # Flow rate (L/min)
         self.Vr = Vr  # Reactor volume (L)
-        self.Qmax = Qmax  # Maximum adsorption capacity (mg/g)
+        self.Qmax = Qmax  # Max adsorption capacity (mg/g)
         self.KL = KL  # Langmuir constant (L/mg)
+        self.k2 = k2  # Pseudo-second-order rate constant
+        self.L = L  # Contactor length
+        self.Mm = Mm  # Molecular weight or characteristic parameter
+        self.tspan = tspan  # Time span for simulation
 
-        # mass transfer coeff
-        self.k_ext = k_ext
-        self.k_int = k_int
+    def reactor(self):
+        """Solves the mass balance ODE"""
+        t = np.linspace(0, self.tspan, self.tspan + 1)
+        sol = integrate.odeint(self.mass_balance, [self.C0, self.qt0], t)
+        return t, sol[:, 0], sol[:, 1]  # Return time, concentration, and qt
 
-        self.A = A # surface area
+    def mass_balance(self, cc, t):
+        """Defines the system of ODEs for concentration and adsorption"""
+        c, qt = cc
 
-    def reactor(self, tspan):
-        t = np.linspace(0, tspan, tspan + 1)
+        qe = self.langmuir(c)
 
-        # Solve for Ce (outlet concentration)
-        c_sol = integrate.odeint(self.mass_balance, self.C0, t)
+        if qe == 0:
+            return [0, 0]
 
-        return t, c_sol.flatten()
+        # a = 1 / (self.k2 * qe)
+        # initRate = 1 / a
+        # rate = initRate * (1 - qt / qe)
 
-    def mass_balance(self, C, tspan):
-        # Calculate adsorption capacity at equilibrium
-        q_ads = self.langmuir(C)
+        # pseudo second order
+        dqdt = (self.k2 * qe**2) / (1 + self.k2 * qe * t)
 
-        # equilibrium concentration
-        Ceq = self.langmuir_inverse(q_ads)
+        dcdt = self.Fv * (self.C0 - c) / self.Vr - dqdt * self.L / self.Mm
+        # dqdt = rate
 
-        # external mass transfer
-        ext_mf = (self.k_ext * self.A *(C - Ceq)) / self.Vr
-
-        # internal mass transfer
-        int_mf = (self.k_int * self.A *(C - Ceq)) / self.Vr
-
-        # dCdt
-        dCdt = - ext_mf - int_mf
-
-        return dCdt
+        return [dcdt, dqdt]
 
     def langmuir(self, Ce):
+        """Langmuir isotherm model"""
         return (self.Qmax * self.KL * Ce) / (1 + self.KL * Ce)
     
-    def langmuir_inverse(self, q_ads):
-        return q_ads / (self.KL * self.Qmax  - q_ads)
+    def plot_result(self):
+        """Runs the simulation and plots results"""
+        t, c_sol, qt_sol = self.reactor()
+        plt.figure(figsize=(8, 5))
+        plt.plot(t, c_sol, label="C (mg/L)", color='b')
+        plt.plot(t, qt_sol, label="qt (mg/g)", color='r')
+        plt.xlabel("Time (min)")
+        plt.ylabel("Concentration & Adsorption")
+        plt.legend()
+        plt.title("Adsorption Process Over Time")
+
+        save_fig = os.path.join(os.getcwd(), "concentration_time.png")
+        plt.savefig(save_fig)
